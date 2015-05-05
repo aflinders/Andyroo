6298e30e2743e323f5544264927d31b6b2bd7085
aflinders <aflinder_99@yahoo.com>
2015-05-05 16:30:25 -0400

    Adds Tour+ view to tab bar with icon and TourViewController class.

---
+++ Andyroo/TourViewController.h
@@ -0,0 +1,13 @@
+//
+//  TourViewController.h
+//  Andyroo
+//
+//  Created by Andrew Flinders on 5/5/15.
+//  Copyright (c) 2015 Andyroo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TourViewController : UIViewController
+
+@end
